141d272fdfd0c425ddeb66fe0e727ef36b75c280
TA2k <tombox2020@gmail.com>
2020-04-10 22:46:46 +0200

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "gartenbewaesserung adapter settings": {
+        "en": "Adapter settings for gartenbewaesserung",
+        "de": "Adaptereinstellungen für gartenbewaesserung",
+        "ru": "Настройки адаптера для gartenbewaesserung",
+        "pt": "Configurações do adaptador para gartenbewaesserung",
+        "nl": "Adapterinstellingen voor gartenbewaesserung",
+        "fr": "Paramètres d'adaptateur pour gartenbewaesserung",
+        "it": "Impostazioni dell'adattatore per gartenbewaesserung",
+        "es": "Ajustes del adaptador para gartenbewaesserung",
+        "pl": "Ustawienia adaptera dla gartenbewaesserung",
+        "zh-cn": "gartenbewaesserung的适配器设置"
+    },
+    "option1": {
+        "en": "option1",
+        "de": "Option 1",
+        "ru": "Опция 1",
+        "pt": "Opção 1",
+        "nl": "Optie 1",
+        "fr": "Option 1",
+        "it": "opzione 1",
+        "es": "Opción 1",
+        "pl": "opcja 1",
+        "zh-cn": "选项1"
+    },
+    "option2": {
+        "en": "option2",
+        "de": "Option 2",
+        "ru": "option2",
+        "pt": "opção 2",
+        "nl": "Optie 2",
+        "fr": "Option 2",
+        "it": "opzione 2",
+        "es": "opcion 2",
+        "pl": "Opcja 2",
+        "zh-cn": "选项2"
+    }
+};
---
+++ main.js
@@ -0,0 +1,84 @@
+"use strict";
+
+/*
+ * Created with @iobroker/create-adapter v1.23.0
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
+
+// Load your modules here, e.g.:
+// const fs = require("fs");
+
+class Gartenbewaesserung extends utils.Adapter {
+
+    /**
+     * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: "gartenbewaesserung",
+        });
+        this.on("ready", this.onReady.bind(this));
+        this.on("stateChange", this.onStateChange.bind(this));
+        this.on("unload", this.onUnload.bind(this));
+    }
+
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        // Initialize your adapter here
+        // Reset the connection indicator during startup
+        this.setState("info.connection", false, true);
+        // in this template all states changes inside the adapters namespace are subscribed
+        this.subscribeStates("*");
+
+      
+    }
+
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            this.log.info("cleaned everything up...");
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+    onStateChange(id, state) {
+        if (state) {
+            // The state was changed
+            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+        } else {
+            // The state was deleted
+            this.log.info(`state ${id} deleted`);
+        }
+    }
+
+
+}
+
+// @ts-ignore parent is a valid property on module
+if (module.parent) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<ioBroker.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new Gartenbewaesserung(options);
+} else {
+    // otherwise start the instance directly
+    new Gartenbewaesserung();
+}
