b1c114ffc0aa93088a118cbe2113478758010f25
TA2k <tombox2020@gmail.com>
2020-04-13 00:25:03 +0200

    added bewässernug auto mode

--- main.js
+++ main.js
@@ -7,6 +7,8 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require("@iobroker/adapter-core");
+const SunCalc = require("suncalc");
+const moment = require("moment");
 
 // Load your modules here, e.g.:
 // const fs = require("fs");
@@ -35,8 +37,14 @@ class Gartenbewaesserung extends utils.Adapter {
         // in this template all states changes inside the adapters namespace are subscribed
 
         this.clockInterval = null;
+        this.pauseTime = 5000;
+        this.bewaesserung_automatik = false;
+        this.ventileTimeouts = [];
+        this.activeVentil = { name: "", end: "", dauer: 0 };
+        this.bewaesserungEnd = "";
         this.getWeatherAndSunInterval = null;
-
+        this.currentLong = "13.404954";
+        this.curentLang = "52.520008";
         this.ventile = [];
         if (this.config.ventil1_enable) {
             this.ventile.push({
@@ -92,10 +100,10 @@ class Gartenbewaesserung extends utils.Adapter {
         }
         this.ventile.forEach(async (item, index) => {
             index++;
-            let stringIndex = index.toString();
-            if (index < 10) {
-                stringIndex = "0" + index;
-            }
+            const stringIndex = index.toString();
+            // if (index < 10) {
+            //     stringIndex = "0" + index;
+            // }
             await this.setObjectNotExistsAsync("config.ventil" + stringIndex, {
                 type: "device",
                 common: {
@@ -179,6 +187,10 @@ class Gartenbewaesserung extends utils.Adapter {
             { name: "lautzeit_gesamt_in_sek", type: "number", unit: "sek" },
             { name: "restzeit_sek", type: "number", unit: "sek" },
             { name: "fortschritt", type: "number", unit: "%" },
+            { name: "tempforecast", type: "number", unit: "°c" },
+            { name: "rainforecast", type: "number", unit: "mm" },
+            { name: "sonnenaufgang", type: "string", unit: "Uhr" },
+            { name: "sonnenuntergang", type: "string", unit: "Uhr" },
         ];
         for (const property of status) {
             await this.setObjectNotExistsAsync("status." + property.name, {
@@ -196,7 +208,11 @@ class Gartenbewaesserung extends utils.Adapter {
         }
         const controls = [
             { name: "bewaesserung_aktiv", type: "boolean", unit: "" },
-            { name: "bewaesserung_pause", type: "boolean", unit: "" },
+            // { name: "bewaesserung_pause", type: "boolean", unit: "" },
+            { name: "ventil1_aktiv", type: "boolean", unit: "" },
+            { name: "ventil2_aktiv", type: "boolean", unit: "" },
+            { name: "ventil3_aktiv", type: "boolean", unit: "" },
+            { name: "ventil4_aktiv", type: "boolean", unit: "" },
         ];
         for (const property of controls) {
             await this.setObjectNotExistsAsync("control." + property.name, {
@@ -213,31 +229,135 @@ class Gartenbewaesserung extends utils.Adapter {
             });
         }
         this.stopVentile();
-        this.getWeatherAndSunData();
+
+        await this.getForeignObjectAsync("system.config").then((obj) => {
+            if (obj && obj.common && obj.common.longitude) {
+                this.currentLong = obj.common.longitude;
+                this.currentLat = obj.common.latitude;
+            } else {
+                this.log.warn("No Lat and Long in ioBroker settings found. Use city Berlin for sunrise and sunset.");
+            }
+        });
+        await this.getWeatherAndSunData();
+        this.checkForBewaesserungStart();
         this.subscribeStates("*");
+
+        this.getWeatherAndSunInterval = setInterval(() => {
+            this.getWeatherAndSunData();
+        }, 30 * 60 * 1000); //30min
+        this.clockInterval = setInterval(() => {
+            this.checkForBewaesserungStart();
+            this.updateVentileStatus();
+        }, 10 * 1000); //10sec
     }
-    async getWeatherAndSunData() {
-        const states = [
-            { name: "sonnenaufgang", type: "string", unit: "Uhr" },
-            { name: "sonnenuntergang", type: "string", unit: "Uhr" },
-            { name: "maxTempVorhersage", type: "number", unit: "°c" },
-            { name: "regenMengeVorhersage", type: "number", unit: "mm" },
-        ];
-        for (const state of states) {
-            await this.setObjectNotExistsAsync("status." + state.name, {
-                type: "state",
-                common: {
-                    name: state.name,
-                    role: "indicator",
-                    type: state.type,
-                    write: false,
-                    read: true,
-                    unit: state.unit || "",
-                },
-                native: {},
-            });
+    async checkForBewaesserungStart() {
+        if (!this.bewaesserung_automatik) {
+            if (this.config.sonnenstand) {
+                const sunrise = moment(this.times.sunrise).add(this.config.minSonnenaufgang, "minute");
+                const sunset = moment(this.times.sunset).add(this.config.minSonnenuntergang, "minute");
+                if (moment().isSame(sunrise, "minute") || moment().isSame(sunset, "minute")) {
+                    this.startBewaesserung();
+                }
+            }
+        }
+        if (
+            (this.config.startzeit1_enable && moment().isSame(moment(this.config.startzeit1, "hh:mm"), "minute")) ||
+            (this.config.startzeit2_enable && moment().isSame(moment(this.config.startzeit2, "hh:mm"), "minute")) ||
+            (this.config.startzeit3_enable && moment().isSame(moment(this.config.startzeit3, "hh:mm"), "minute"))
+        ) {
+            this.startBewaesserung();
         }
     }
+    async updateVentileStatus() {
+        if (this.activeVentil && this.activeVentil.name) {
+            this.setState("status." + this.activeVentil.name + ".restzeit", moment().subtract(this.activeVentil.end).minute());
+            this.setState("status." + this.activeVentil.name + ".restzeit_sek", moment().subtract(this.activeVentil.end).second());
+            this.setState("status." + this.activeVentil.name + ".fortschritt", (this.activeVentil.dauer * 100) / moment().subtract(this.activeVentil.end).second());
+        }
+        if (this.bewaesserungEnd) {
+            this.setState("status.restzeit", moment().subtract(this.bewaesserungEnd).minute());
+            this.setState("status.restzeit_sek", moment().subtract(this.bewaesserungEnd).second());
+            this.setState("status.fortschritt", ((this.currentTimeoutTime / 1000) * 100) / moment().subtract(this.bewaesserungEnd).second());
+        }
+    }
+    async startBewaesserung() {
+        await this.stopBewaesserung();
+        this.bewaesserung_automatik = true;
+        this.setState("status.bewaesserung_automatik", true, true);
+        this.currentTimeoutTime = 0;
+        for (const ventil in this.ventile) {
+            let dauer = ventil.dauer * 60;
+            if (ventil.dauer_sec) {
+                dauer = ventil.dauer;
+            }
+            const stopTime = this.currentTimeoutTime + dauer * 1000;
+            this.log.info("Start " + ventil.id + " in " + this.currentTimeoutTime / 1000 + "sek");
+            const timeoutIdStart = setTimeout(() => {
+                const end = moment().add(stopTime, "millisecond");
+                this.log.info("Start " + ventil.id);
+                this.setState(ventil.state, true, false);
+                this.setState("status." + ventil.id + ".active", true, false);
+                this.setState("status." + ventil.id + ".ende", end.toLocaleString(), false);
+                this.activeVentil.name = ventil.id;
+                this.activeVentil.end = end;
+                this.activeVentil.dauer = dauer;
+            }, this.currentTimeoutTime);
+            this.ventileTimeouts.push(timeoutIdStart);
+
+            this.log.info("Stop " + ventil.id + " in " + stopTime / 1000 + "sek");
+            const timeoutIdStop = setTimeout(() => {
+                this.log.info("Stop " + ventil.id);
+                this.setState(ventil.state, false, false);
+
+                this.setState("status." + ventil.id + ".active", false, false);
+                this.setState("status." + ventil.id + ".ende", "", false);
+                this.activeVentil.name = "";
+                this.activeVentil.end = "";
+                this.activeVentil.dauer = 0;
+            }, stopTime);
+            this.ventileTimeouts.push(timeoutIdStop);
+            this.currentTimeoutTime = stopTime + this.pauseTime;
+        }
+        this.bewaesserungEnd = moment().add(this.currentTimeoutTime, "millisecond");
+
+        this.setState("status.lautzeit_ende_uhrzeit", this.bewaesserungEnd.toLocaleString());
+        this.setState("status.lautzeit_gesamt_in_sek", this.bewaesserungEnd.seconds());
+    }
+    stopBewaesserung() {
+        return new Promise(async (resolve, reject) => {
+            this.stopVentile();
+            this.bewaesserungEnd = "";
+            for (const timeout in this.ventileTimeouts) {
+                clearTimeout(timeout);
+            }
+            this.bewaesserung_automatik = false;
+            await this.setStateAsyncs("status.bewaesserung_automatik", false, true);
+            resolve();
+        });
+    }
+    async getWeatherAndSunData() {
+        return new Promise(async (resolve, reject) => {
+            if (this.config.tempforecast) {
+                await this.getForeignStateAsync(this.config.tempforecast)
+                    .then((obj) => {
+                        obj && this.setState("status.tempforecast", obj.val, true);
+                    })
+                    .catch((error) => this.log.error("Cannot receive temp forecast from:" + this.config.tempforecast + " " + JSON.stringify(error)));
+            }
+            if (this.config.rainforecast) {
+                await this.getForeignStateAsync(this.config.rainforecast)
+                    .then((obj) => {
+                        obj && this.setState("status.rainforecast", obj.val, true);
+                    })
+                    .catch((error) => this.log.error("Cannot receive rain forecast from:" + this.config.rainforecast + " " + JSON.stringify(error)));
+            }
+            this.times = SunCalc.getTimes(new Date(), this.currentLat, this.currentLong);
+            this.setState("status.sonnenaufgang", this.times.sunrise.toTimeString());
+            this.setState("status.sonnenuntergang", this.times.sunset.toTimeString());
+            resolve();
+        });
+    }
+
     stopVentile() {
         this.ventile &&
             this.ventile.forEach(async (item) => {
